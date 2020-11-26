8e1e2c22fff9e2513a627f379949b1b382fb0477
somerk21 <cmtuta@misena.edu.co>
2020-11-26 15:26:27 -0500

    aplicacion

---
+++ Scripts/doot.js
@@ -0,0 +1,10 @@
+﻿function DootDown() {
+    document.getElementById('imgDoot').setAttribute('src', 'media/tambor.jpg');
+
+    var audio = new Audio('media/audio.wav');
+    audio.play();
+}
+
+function DootUp() {
+    document.getElementById('imgDoot').setAttribute('src', 'media/PLATILLOS.jpg')
+}
